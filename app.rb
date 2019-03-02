da9713b67bfa2247af59f66c50e758aa5c432b14
Ashley Wong <ashwong04@gmail.com>
2019-03-02 21:43:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   end
   
   post '/checkout' do
-    binding.pry
+    # binding.pry
     @cart= params
     @session['item']=params['item'].first.last
     @session= session
