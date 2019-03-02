4e57db44e87eb80ff8f789851b816e837fcb5e2a
Ashley Wong <ashwong04@gmail.com>
2019-03-02 21:50:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,7 +22,10 @@ class App < Sinatra::Base
   
 end
 
-
+# get '/hey' do 
+#   session["name"] = "Victoria"
+#   @session = session
+# end
 
 # {"session_id"=>"468b07f2c789131dbe304c07d0479a5478dbec32088efb21b9d245ee391f891c", 
 # "csrf"=>"chr/+3+1efA6wW7KDabsLjuMT9vcIVehiu5nGSMzQ5M=", 
